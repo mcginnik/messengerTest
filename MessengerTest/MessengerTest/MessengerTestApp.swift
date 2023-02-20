286b096967bf15d62ffa915d06265643c39ffe9b
Kyle McGinnis <kyle.mcginnis@gmail.com>
2023-02-20 07:17:21 -0800

    initial swiftui project

---
+++ MessengerTest/MessengerTest/MessengerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTestApp.swift
+//  MessengerTest
+//
+//  Created by Kyle McGinnis on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
